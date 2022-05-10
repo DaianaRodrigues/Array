31e5369b5a5da4ca2b4b98eb55105bb327e65671
DaianaRodrigues <102737592+DaianaRodrigues@users.noreply.github.com>
2022-05-10 09:38:18 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+//let notaHtml = 10;
+//let notaCSS = 10;
+//let notaJS = 10;
+
+//let media = ((notaHtml+notaCSS+notaJS)/3)
+//console.log(media)
+
+let notas = [9, 5, 6]
+
+console.log(notas)
+console.log(notas.length)
+
+let media = (notas[0]+notas[1]+notas[2])/notas.length
+console.log(media)
---
+++ arrayAdd.js
@@ -0,0 +1,8 @@
+const notas = [10, 2, 8]
+notas.push(7)
+console.log(notas)
+
+//média
+
+const media = (notas[0]+ notas[1] +notas[2] +notas[3])/4;
+console.log(media)
